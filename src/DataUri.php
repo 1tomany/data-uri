a6b64b4e270a0f3afc50d0ed179462eb7154bd32
Vic Cherubini <vic@1tomany.com>
2024-12-17 10:02:43 -0600

    wip

--- src/DataUri.php
+++ src/DataUri.php
@@ -2,6 +2,23 @@
 
 namespace OneToMany\DataUri;
 
+use finfo;
+
+use function base64_decode;
+use function explode;
+use function file_get_contents;
+use function file_put_contents;
+use function filesize;
+use function is_file;
+use function is_readable;
+use function mime_content_type;
+use function sha1_file;
+use function substr;
+use function sys_get_temp_dir;
+use function unlink;
+use const FILEINFO_EXTENSION;
+use const FILEINFO_MIME_TYPE;
+
 final readonly class DataUri
 {
 
@@ -10,8 +27,8 @@ final readonly class DataUri
         public string $hash,
         public string $mimeType,
         public string $filename,
-        public string $extension,
-        public int $size,
+        public ?string $extension = null,
+        public int $size = 0,
     )
     {
     }
@@ -20,6 +37,59 @@ final readonly class DataUri
     {
         if (null === $data) {
             throw new \InvalidArgumentException('null file');
+        }
+
+        $bytes = null;
+
+        if (is_file($data) && is_readable($data)) {
+            $bytes = @file_get_contents($data);
+        }
+
+        if (str_starts_with($data, 'data:')) {
+            // Remove 'data:' Prefix and Break Apart
+            $bits = explode(',', substr($data, 5), 2);
+
+            // Decode to Binary Representation
+            if (null !== ($bits[1] ?? null)) {
+                $bytes = base64_decode($bits[1]);
+            }
+        }
+
+        if (!is_string($bytes)) {
+            throw new \InvalidArgumentException('cant get bytes');
+        }
+
+        try {
+            $pathname = @tempnam(sys_get_temp_dir(), '__1n__datauri_');
+
+            if (false === $pathname || !is_file($pathname)) {
+                throw new \RuntimeException('no path');
+            }
+
+            $bytes = file_put_contents($pathname, $bytes);
+
+            if (false === $bytes) {
+                throw new \RuntimeException('no bytes written');
+            }
+
+            // $size = strlen($bytes);
+            // $hash = sha1($bytes);
+            $info = new finfo();
+
+            if (false === $mimeType = $info->file($pathname, FILEINFO_MIME_TYPE)) {
+                throw new \RuntimeException('no mime type');
+            }
+
+            if (false === $extension = $info->file($pathname, FILEINFO_EXTENSION)) {
+                throw new \RuntimeException('no extension found');
+            }
+
+        } catch (\Throwable $e) {
+            if (is_file($pathname)) {
+                @unlink($pathname);
+            }
+
+            throw $e;
         }
     }
 
---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace OneToMany\DataUri\Exception;
+
+interface ExceptionInterface extends \Throwable
+{
+}
