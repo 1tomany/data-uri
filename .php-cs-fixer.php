768b6855b3faf7ce8adcd0905329317a630b35da
Vic Cherubini <vcherubini@gmail.com>
2025-03-31 09:29:47 -0500

    cleanup

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,28 +2,26 @@
 
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
-use PhpCsFixer\Fixer\Phpdoc\PhpdocSeparationFixer;
 
-$finder = PhpCsFixer\Finder::create()
-    ->in('./src')
-    // ->exclude('folder-to-exclude') // Optional: exclude folders
-    // ->notPath('file-to-exclude.php') // Optional: exclude specific files
-;
+$finder = PhpCsFixer\Finder::create()->in('./src');
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         'class_attributes_separation' => [
             'elements' => [
-            // 'const' => 'one', 'method' => 'one', 'property' => 'one', 'trait_import' => 'none', 'case' => 'none',
+                'case' => 'none',
+                'const' => 'one',
+                'property' => 'one',
+                'trait_import' => 'none',
             ],
         ],
+        'no_blank_lines_after_class_opening' => false,
         'no_extra_blank_lines' => [
             'tokens' => [
                 'attribute',
                 'case',
                 'continue',
-                // 'curly_brace_block',
                 'default',
                 'extra',
                 'parenthesis_brace_block',
@@ -33,17 +31,8 @@ return (new PhpCsFixer\Config())
                 'use',
             ],
         ],
-
-
-        // '@PER-CS2.0' => true,
-        'no_blank_lines_after_class_opening' => false,
-
-
-
-
         'ordered_types' => [
             'null_adjustment' => 'always_first',
         ],
     ])
-    ->setFinder($finder)
-;
+    ->setFinder($finder);
