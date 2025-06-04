8ecb8271dc0d34a4728ff049873c6b11380486c4
Vic Cherubini <vcherubini@gmail.com>
2025-06-04 18:42:53 -0500

    wip

--- tests/ParseDataTest.php
+++ tests/ParseDataTest.php
@@ -14,6 +14,7 @@ use Symfony\Component\Filesystem\Filesystem;
 use function assert;
 use function base64_encode;
 use function basename;
+use function OneToMany\DataUri\parse_base64_data;
 use function OneToMany\DataUri\parse_data;
 use function random_bytes;
 use function sys_get_temp_dir;
@@ -206,5 +207,30 @@ final class ParseDataTest extends TestCase
             [__DIR__.'/data/text-small.txt', 86, 'text/plain'],
             [__DIR__.'/data/word-small.docx', 6657, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
         ];
+    }
+
+    #[DataProvider('providerBase64DataAndMetadata')]
+    public function testParsingBase64Data(string $data, int $size, string $type): void
+    {
+        $file = parse_base64_data($data, $type);
+
+        $this->assertFileExists($file->path);
+        $this->assertEquals($size, $file->size);
+        $this->assertEquals($type, $file->type);
+    }
+
+    /**
+     * @return list<list<int|non-empty-string>>
+     */
+    public static function providerBase64DataAndMetadata(): array
+    {
+        $provider = [
+            ['eyJpZCI6MTB9', 9, 'application/json'],
+            ['SGVsbG8sIHdvcmxkIQ==', 13, 'text/plain'],
+            ['R0lGODdhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==', 43, 'image/gif'],
+            ['iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQImWNgAAIAAAUAAWJVMogAAAAASUVORK5CYII=', 68, 'image/png'],
+        ];
+
+        return $provider;
     }
 }
