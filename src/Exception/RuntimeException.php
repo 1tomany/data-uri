e7e293190c949eb8f8dcb641f0ab1ed79e5080b1
Vic Cherubini <vcherubini@gmail.com>
2025-07-25 21:50:46 -0500

    [#15] Create a `parse_text_data()` function to convert a string of text to a `SmartFile` (#16)
    resolves #15

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -2,6 +2,8 @@
 
 namespace OneToMany\DataUri\Exception;
 
+use OneToMany\DataUri\Contract\Exception\ExceptionInterface;
+
 class RuntimeException extends \RuntimeException implements ExceptionInterface
 {
 }
