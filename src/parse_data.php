99e31adcf1f089f8042e8941247758a063a30f8e
Vic Cherubini <vcherubini@gmail.com>
2025-12-04 14:48:46 -0600

    wip

--- src/parse_data.php
+++ src/parse_data.php
@@ -27,6 +27,7 @@ use function mime_content_type;
 use function parse_url;
 use function pathinfo;
 use function random_bytes;
+use function rtrim;
 use function sprintf;
 use function str_contains;
 use function str_ends_with;
