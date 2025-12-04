b3c32c7df133b3dc061d441ea106818f43658a38
Vic Cherubini <vcherubini@gmail.com>
2025-12-03 22:11:30 -0600

    wip

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -29,9 +29,9 @@ $config->setRules([
         'import_constants' => true,
         'import_functions' => true,
     ],
-    // 'phpdoc_align' => [
-    //     'align' => 'left',
-    // ],
+    'phpdoc_align' => [
+        'align' => 'left',
+    ],
     'phpdoc_to_comment' => [
         'ignored_tags' => [
             'disregard',
