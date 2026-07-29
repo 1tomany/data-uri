a7c2c61ac77e6b03c3dcb434743a96eaf6179601
Vic Cherubini <vcherubini@gmail.com>
2026-07-29 15:14:35 -0500

    wip

---
+++ src/Contract/Record/TemporaryFileInterface.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace OneToMany\DataUri\Contract\Record;
+
+use OneToMany\DataUri\Contract\Enum\Type;
+use OneToMany\DataUri\Exception\RuntimeException;
+
+interface TemporaryFileInterface extends \Stringable
+{
+    /**
+     * Minimum hash length to ensure a key can be generated.
+     *
+     * @var positive-int
+     */
+    public const int MINIMUM_HASH_LENGTH = 4;
+
+    /**
+     * @return non-empty-lowercase-string
+     */
+    public function getHash(): string;
+
+    /**
+     * @return non-empty-string
+     */
+    public function getPath(): string;
+
+    /**
+     * @return ?non-empty-string
+     */
+    public function getRoot(): ?string;
+
+    /**
+     * @return non-empty-string
+     */
+    public function getName(): string;
+
+    /**
+     * @return non-negative-int
+     */
+    public function getSize(): int;
+
+    public function getType(): Type;
+
+    /**
+     * @return ?non-empty-lowercase-string
+     */
+    public function getExtension(): ?string;
+
+    /**
+     * @return non-empty-lowercase-string
+     */
+    public function getFormat(): string;
+
+    /**
+     * @return non-empty-string
+     */
+    public function getKey(): string;
+
+    /**
+     * Determines if two instances are equal.
+     */
+    public function equals(self $file, bool $strict = false): bool;
+
+    /**
+     * Determines if the file the object represents exists.
+     */
+    public function exists(): bool;
+
+    /**
+     * Reads the entire file into memory.
+     *
+     * @throws RuntimeException when reading the file fails
+     */
+    public function read(): string;
+
+    /**
+     * Returns the base64 encoding of the file.
+     *
+     * @throws RuntimeException when reading or encoding the file fails
+     */
+    public function toBase64(): string;
+
+    /**
+     * Returns the contents of file as a data URI as defined in RFC 2397.
+     *
+     * @link https://www.rfc-editor.org/rfc/rfc2397.html
+     *
+     * @return non-empty-string
+     *
+     * @throws RuntimeException when reading or encoding the file fails
+     */
+    public function toDataUri(): string;
+
+    /**
+     * Deletes all files and directories represented by this object.
+     *
+     * @throws RuntimeException when any part of the deletion process fails
+     */
+    public function delete(): void;
+
+    /**
+     * Transfers cleanup responsibility to the caller and returns the path.
+     *
+     * @return non-empty-string
+     */
+    public function detach(): string;
+
+    /**
+     * If true, all files and directories represented by the object will be automatically deleted when the destructor is called. If false, the caller is responsible for deleting any filesystem references.
+     */
+    public function isManaged(): bool;
+}
